0f3f7064b51080d1fa35714eb1c8cbdf3c097742
shirishkumart <shirishkumarthi@gmail.com>
2017-05-05 15:27:48 +0530

    adding script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,25 @@
 from fabric.api import local
 
 def test():
-    local("cd /home/devops/fabscript/fabric-test && touch file1 file2 file3" )
-    local("cd /home/devops/fabscript/fabric-test && git add . && git commit -m 'test 123 '" )
+    local("./manage.py")
+
+   # local("cd /home/devops/fabscript/fabric-test && touch file1 file2 file3" )
+def commit():
+    local("cd /home/devops/fabscript/fabric-test && git add . && git commit -m 'adding script'" )
+def push():
     local("cd /home/devops/fabscript/fabric-test && git push origin master ")
+def pull():
+    local(" git pull")
+
+def depoly():
+    print "Executing manage script."
+    print "----------------------------"
+    test()
+    print "Commiting got code locally."
+    print "------------------------------"
+    commit()
+    print "Pushing local git repository to Github."
+    print "---------------------------------------------"
+    push()
+    print "Pull from repository"
+    pull()
